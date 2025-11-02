868ec87ac384d6e2f9595483b131f6a5ddc7f61c
MarkHughes18 <157494982+MarkHughes18@users.noreply.github.com>
2025-11-02 16:16:30 +0000

    MainActivity class

--- app/src/main/java/com/example/finalyearproject/MainActivity.java
+++ app/src/main/java/com/example/finalyearproject/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.finalyearproject;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +17,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button btnSignUp = findViewById(R.id.btnSignUp);
+        Button btnLogIn = findViewById(R.id.btnLogIn);
+
+        btnSignUp.setOnClickListener(v ->
+                startActivity(new Intent(MainActivity.this, SignUpActivity.class)));
+
+        btnLogIn.setOnClickListener(v ->
+                startActivity(new Intent(MainActivity.this, LogInActivity.class)));
 
     }
 }
